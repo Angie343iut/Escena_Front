1300152e25c79db01ab47d6e48752ade5d8eac09
Angie <anguiemaria94@gmail.com>
2026-05-15 09:05:53 -0500

    Add files via upload

---
+++ constants/colors.js
@@ -0,0 +1,10 @@
+export const Colors = {
+  background: '#0D0D0D',
+  card: '#1A1A1A',
+  primary: '#C6FF00',
+  secondary: '#FFD600',
+  text: '#FFFFFF',
+  textMuted: '#888888',
+  border: '#2A2A2A',
+  input: '#1F1F1F',
+};
